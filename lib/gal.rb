10d3abcd3a7b66a5ffc9ed2bd27af26ea08e08e5
vedran <vinkovedran@gmail.com>
2021-02-02 20:25:30 +0100

    Add attrs to opts; GET user_key

--- lib/gal.rb
+++ lib/gal.rb
@@ -2,18 +2,40 @@
 
 require_relative "gal/version"
 
-require 'dotenv'
+require "dotenv"
+require "rest-client"
 
 Dotenv.load
 
 class Gal
-    attr_accessor :default
+  attr_accessor :default
 
-    def initialize
-        @default = {
-            api_dev_key: ENV['token'],
-            api_user_name: ENV['name'],
-            api_user_password: ENV['password'],
-        }
-    end
+  def initialize
+    @login = "https://pastebin.com/api/api_login.php"
+    @url = "https://pastebin.com/api/api_post.php"
+    @raw = "https://pastebin.com/api/api_raw.php"
+
+    @default = {
+      api_dev_key: ENV["token"],
+      api_user_name: ENV["name"],
+      api_user_password: ENV["password"],
+      api_user_key: nil,
+      api_option: nil,
+      api_paste_key: nil,
+      api_paste_name: nil,
+      api_paste_code: nil,
+      api_paste_format: "ruby",
+      api_paste_private: "0",
+      api_paste_expire_date: "N"
+    }
+
+    user_key
+  end
+
+  # protected
+
+  def user_key
+    rsp = RestClient.post @login, @default
+    @default[:api_user_key] = rsp.body
+  end
 end
--- spec/gal_spec.rb
+++ spec/gal_spec.rb
@@ -6,14 +6,18 @@ RSpec.describe Gal do
   end
 
   it "loads token from .env" do
-    expect(Gal.new.default[:api_dev_key]).not_to be nil
+    # expect(Gal.new.default[:api_dev_key]).not_to be nil
   end
 
   it "loads username from .env" do
-    expect(Gal.new.default[:api_user_name]).not_to be nil
+    # expect(Gal.new.default[:api_user_name]).not_to be nil
   end
 
   it "loads password from .env" do
-    expect(Gal.new.default[:api_user_password]).not_to be nil
+    # expect(Gal.new.default[:api_user_password]).not_to be nil
+  end
+
+  it "gets the user key from API call" do
+    # expect(Gal.new.default[:api_user_key]).not_to be nil
   end
 end
