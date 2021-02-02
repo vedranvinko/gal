eed569da04351836b307dd73bcb42c0a2f1308a9
vedran <vinkovedran@gmail.com>
2021-02-02 14:39:22 +0100

    Add attrs to default opts for initialize

--- lib/gal.rb
+++ lib/gal.rb
@@ -2,5 +2,18 @@
 
 require_relative "gal/version"
 
+require 'dotenv'
+
+Dotenv.load
+
 class Gal
+    attr_accessor :default
+
+    def initialize
+        @default = {
+            api_dev_key: ENV['token'],
+            api_user_name: ENV['name'],
+            api_user_password: ENV['password'],
+        }
+    end
 end
--- spec/gal_spec.rb
+++ spec/gal_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Gal do
   it "has a version number" do
     expect(Gal::VERSION).not_to be nil
   end
+
+  it "loads token from .env" do
+    expect(Gal.new.default[:api_dev_key]).not_to be nil
+  end
+
+  it "loads username from .env" do
+    expect(Gal.new.default[:api_user_name]).not_to be nil
+  end
+
+  it "loads password from .env" do
+    expect(Gal.new.default[:api_user_password]).not_to be nil
+  end
 end
